4daaf0be672c11e901c575d4c3c7c768fc1697ac
John Maya <jcmaya@hotmail.com>
2015-01-08 17:42:09 -0500

    fixed create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
-def new
-  @user = User.new
-end
-
-def create
-  @user = User.new(params[:user])
-  if @user.save
-    redirect_to root_url, :notice => "Signed Up!"
-  else
-    render "new"
+  def new
+    @user = User.new
   end
-end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, :notice => "Signed Up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email,:password,:password_confirmation)
+  end
+
 end
